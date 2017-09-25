31fc79a647cd936abb4dde520bbb8677ad5693df
Paul Williams <paul@paulswilliams.me.uk>
2017-09-25 21:25:42 +0100

    Moved static content to data, and extracted react component

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png"
+    }
+  },
+  {
+    'title': "Portfolio boilerplate",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example2.png"
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png"
+    }
+  }
+];
 
-console.log("Webpack works");
-
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
